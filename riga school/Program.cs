d289ba27445f5e96e21d2a896bece1cf7c50790a
mary- <mary-@DESKTOP-4AMCLHE>
2021-10-07 20:55:20 +0300

    comment

--- riga school/Program.cs
+++ riga school/Program.cs
@@ -9,6 +9,7 @@ namespace riga_school
             Console.WriteLine("Hello World!" +
                 "My name is Marina,\n spend 13 h per day on computer,\nthats why I can`t wright down the long text for my home work \n I have a dog, it is preatty. I have a masters degree in Publick relations and advertising.");
             Console.ReadLine();
+            //comment
         }
     }
 }
