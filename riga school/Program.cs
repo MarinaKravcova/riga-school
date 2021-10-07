18dfeed9a2c6652b9a2db90773047c5f336949c9
mary- <mary-@DESKTOP-4AMCLHE>
2021-10-07 20:43:22 +0300

    Add project files.

---
+++ riga school/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace riga_school
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!" +
+                "My name is Marina,\n spend 13 h per day on computer,\nthats why I can`t wright down the long text for my home work \n I have a dog, it is preatty. I have a masters degree in Publick relations and advertising.");
+            Console.ReadLine();
+        }
+    }
+}
