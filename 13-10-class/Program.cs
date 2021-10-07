d3a2dd76535f821075daa79cfde2a325d3b4447b
mary- <mary-@DESKTOP-4AMCLHE>
2021-10-07 21:22:16 +0300

    13-10new comment1

---
+++ 13-10-class/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _13_10_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Math
+
+            Console.WriteLine ("input number:");
+            string inputString = Console.ReadLine();
+            int integerVarable = int.Parse(inputString);
+           decimal number = Convert.ToDecimal(inputString);
+
+            // value % 2 != 0 Odd number
+            // value % 2 != 0 Even number
+            //sxpresion ? value_1 : value_2
+
+            string truOrFalse = (number > 1) ? "It is grater then 1" : "it is not greater then one";
+            Console.WriteLine(); 
+
+            #endregion
+
+        }
+    }
+}
--- riga school/Program.cs
+++ riga school/Program.cs
@@ -6,12 +6,50 @@ namespace riga_school
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!" +
-                "My name is Marina,\n spend 13 h per day on computer,\nthats why I can`t wright down the long text for my home work \n I have a dog, it is preatty. I have a masters degree in Publick relations and advertising.");
-            Console.ReadLine();
-            //comment
-            //comment
-            //comment
+
+            #region Casting
+
+            int integerVariable = 1;
+            long decimalVariable = 12222;
+            decimal decimalVariable = 1;
+            string stringVariable = " 100 "; 
+
+            #endregion
+
+            #region Mynumbers
+            int number1 = 100;
+            int number2 = 1;
+            int result = number1 + number2;
+
+            string number3 = "100";
+            string number4 = "1";
+
+            string stringResult = number3 + number4; //What is the result?
+
+            Console.WriteLine($"int result: {result}");
+            Console.WriteLine($"string result: {stringResult}");
+            #endregion
+
+            #region MyWorld
+
+
+            string helloWorldVariable = "Hello World! line 1.";
+
+            string helloWorldVariableNumber2 = "Hello World! line 2.";
+
+         // Line1
+         Console.WriteLine("Line 1: " + helloWorldVariable);
+            Console.WriteLine(Environment.NewLine);
+
+         // Line2
+         Console.WriteLine("Line 2: " + helloWorldVariableNumber2);
+            Console.WriteLine(Environment.NewLine);
+
+        // Line3
+         Console.WriteLine("Line 3: " + helloWorldVariableNumber2);
+            Console.WriteLine(Environment.NewLine);
+
+         #endregion MyWorld
         }
     }
 }
