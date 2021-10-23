6211aaa03930eae61154b1c01cbc9c6f5e47f059
mary- <mary-@DESKTOP-4AMCLHE>
2021-10-23 20:32:06 +0300

    23.10-1

---
+++ home_work_module_4.3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace home_work_module_4._3
+{
+    class Program
+    {
+        private static string cout;
+
+        static void Main(string[] args)
+        {
+    {
+        int[] arr = { 1, 2, 3, 8, 2, 6, 7 };
+        int n = 6, sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += arr[i];
+        }
+                cout << "The array sum is " << sum;
+        return 0;
+    }
+    }
+    }
+}
---
+++ home_work_module_4.4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace home_work_module_4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strArray = new string[] {
+    "Marakuja",
+    "Lemon Juice",
+    "Gin",
+    "Orange Sirup",
+    "Sparkling water"
+}; 
+           
+            foreach (string str in strArray)
+            {
+                Console.WriteLine(str);
+                Console.ReadLine();
+            }
+        }
+    }
+}
